4ada4581f24c5a394d594e9b63ac82fb0f39659b
lizijing98 <lizijing98@live.com>
2022-12-15 14:48:56 +0800

    所有赛程更新完

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-import xlsx from "node-xlsx";
-import moment from "moment";
-import fs from "fs";
-import flagMap from "./src/flagMap.js";
-import constant from "./src/constant.js";
+import xlsx from 'node-xlsx';
+import moment from 'moment';
+import fs from 'fs';
+import flagMap from './src/flagMap.js';
+import constant from './src/constant.js';
 
-const timeFormat = "YYYYMMDDTHHmmss";
+const timeFormat = 'YYYYMMDDTHHmmss';
 
 // 解析得到文档中的所有 sheet
-var sheets = xlsx.parse("WorldCupSchedule.xlsx", {
+var sheets = xlsx.parse('WorldCupSchedule.xlsx', {
 	cellDates: true,
 });
 
-var calData = constant.BEGIN + constant.VERSION + constant.PRODID + constant.CALSCALE + constant.CALNAME + constant.APPLE_COLOR;
+var calData =
+	constant.BEGIN + constant.VERSION + constant.PRODID + constant.CALSCALE + constant.CALNAME + constant.APPLE_COLOR;
 
 // 遍历 sheet
 sheets.forEach(function (sheet) {
 	// 读取每行内容
-	for (var rowId in sheet["data"]) {
+	for (var rowId in sheet['data']) {
 		if (rowId == 0) {
 			continue;
 		}
-		var row = sheet["data"][rowId];
-		calData += constant.BEGIN_EVENT;
+		var row = sheet['data'][rowId];
 		var group = row[0];
 		var teamA = row[1];
 		var teamB = row[2];
+		if (!group) {
+			break;
+		}
+		calData += constant.BEGIN_EVENT;
 		calData +=
 			constant.SUMMARY +
 			group +
-			"-" +
+			'-' +
 			teamA +
 			(flagMap[teamA] ? flagMap[teamA] : flagMap.other) +
-			"vs" +
+			'vs' +
 			teamB +
 			(flagMap[teamB] ? flagMap[teamB] : flagMap.other) +
-			"\n";
-		var beginTime = moment(new Date(row[6].getTime() + 43 * 1000)).format(timeFormat) + "Z";
-		calData += constant.DTSTART + beginTime + "\n";
-		var endTime = moment(new Date(row[7].getTime() + 43 * 1000)).format(timeFormat) + "Z";
-		calData += constant.DTEND + endTime + "\n";
+			'\n';
+		var beginTime = moment(new Date(row[6].getTime() + 43 * 1000)).format(timeFormat) + 'Z';
+		calData += constant.DTSTART + beginTime + '\n';
+		var endTime = moment(new Date(row[7].getTime() + 43 * 1000)).format(timeFormat) + 'Z';
+		calData += constant.DTEND + endTime + '\n';
 		if (row[5]) {
-			calData += constant.DESCRIPTION + row[5] + "\n" + constant.DESC_TEXT;
+			calData += constant.DESCRIPTION + row[5] + '\n' + constant.DESC_TEXT;
 		} else {
 			calData += constant.DESCRIPTION + constant.DESC_TEXT;
 		}
@@ -50,14 +54,14 @@ sheets.forEach(function (sheet) {
 
 calData += constant.END;
 
-fs.mkdir("./dist", { recursive: true }, (err) => {
+fs.mkdir('./dist', { recursive: true }, (err) => {
 	if (err) throw err;
-	fs.writeFile("./dist/WorldCupSchedule.ics", calData, function (err) {
+	fs.writeFile('./dist/WorldCupSchedule.ics', calData, function (err) {
 		if (err) throw err;
-		console.log("ics created in dist successfully");
+		console.log('ics created in dist successfully');
 	});
 });
-fs.writeFile("./WorldCupSchedule.ics", calData, function (err) {
+fs.writeFile('./WorldCupSchedule.ics', calData, function (err) {
 	if (err) throw err;
-	console.log("ics created successfully");
+	console.log('ics created successfully');
 });
